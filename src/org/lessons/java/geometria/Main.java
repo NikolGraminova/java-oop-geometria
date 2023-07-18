c988d974a21759d99182bcb9e6ca18df38e1c93b
NikolGraminova <nbackup619@gmail.com>
2023-07-18 14:17:39 +0200

    Created project workspace

---
+++ src/org/lessons/java/geometria/Main.java
@@ -0,0 +1,13 @@
+package org.lessons.java.geometria;
+
+/*
+*Creare una classe Main contenente il metodo main(), nel quale chiedete all’utente di inserire, da console, i valori di base e di altezza con cui istanziare un nuovo Rettangolo.
+* Dopo averlo istanziato, stampate a video il perimetro e l’area.
+*BONUS Aggiungere alla classe Rettangolo un metodo disegna() che stampa in console il rettangolo con le sue dimensioni, come nell’immagine allegata
+ */
+
+public class Main {
+    public static void main(String[] args) {
+
+    }
+}
---
+++ src/org/lessons/java/geometria/Rettangolo.java
@@ -0,0 +1,12 @@
+package org.lessons.java.geometria;
+
+/*
+*Nome repo: java-oop-geometria
+*Consegna: Nel progetto java-oop-geometria creare un package org.lessons.java.geometria, in cui inserire le classi:
+*una classe Rettangolo con due attributi interi: base e altezza. Aggiungere un opportuno costruttore con parametri.
+*Aggiungere due metodi: calcolaArea e calcolaPerimetro che calcolano e restituiscono, rispettivamente, l’area e il perimetro del rettangolo.
+ */
+
+
+public class Rettangolo {
+}
